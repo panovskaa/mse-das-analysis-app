1be000c9630fc865f37a422770df748b62e6d319
panovskaa <ele.panovska@gmail.com>
2025-01-19 22:36:17 +0100

    fix

--- Homework 3/stock_exchange_analyst/src/main/java/mk/finki/ukim/mk/stock_exchange_analyst/repository/TAPointRepository.java
+++ Homework 3/stock_exchange_analyst/src/main/java/mk/finki/ukim/mk/stock_exchange_analyst/repository/TAPointRepository.java
@@ -2,10 +2,13 @@ package mk.finki.ukim.mk.stock_exchange_analyst.repository;
 
 import jakarta.annotation.PostConstruct;
 import mk.finki.ukim.mk.stock_exchange_analyst.model.TAPoint;
-import mk.finki.ukim.mk.stock_exchange_analyst.repository.utils.CSVReadingUtil;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
@@ -28,14 +31,14 @@ public class TAPointRepository {
     }
 
     @PostConstruct
-    public void update() throws IOException {
+    public void update() {
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(taPath))) {
             for (Path analysisCSV : stream) {
 
-                List<List<String>> indicators = CSVReadingUtil.readCSV(analysisCSV.toString());
+                List<List<String>> indicators = readCSV(analysisCSV.toString());
                 indicators = indicators.subList(1, indicators.size());
 
-                String companyName = analysisCSV.toString().split("/")[6].split("\\.")[0].split("_")[0];
+                String companyName = analysisCSV.toString().split("\\\\")[4].split("\\.")[0].split("_")[0];
 
                 List<TAPoint> taPoints = indicators.stream()
                         .map(this::createTAPoint)
@@ -44,8 +47,12 @@ public class TAPointRepository {
                         .toList();
 
                 companies.put(companyName, taPoints);
+
             }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
     }
 
     public Optional<TAPoint> latest(String company) {
@@ -56,34 +63,66 @@ public class TAPointRepository {
                 .max(Comparator.comparing(TAPoint::getDate));
     }
 
-    private Optional<TAPoint> createTAPoint(List<String> ls) {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    private List<List<String>> readCSV(String company) {
+        List<List<String>> records = new ArrayList<>();
+
         try {
+
+            FileReader reader = new FileReader(String.format("%s", company));
+            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
+
+            for (CSVRecord csvRecord : csvParser) {
+                List<String> row = new ArrayList<>();
+                csvRecord.forEach(row::add);
+                records.add(row);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return records;
+    }
+
+    private Optional<TAPoint> createTAPoint(List<String> ls) {
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        try {
+
             LocalDate date = LocalDate.parse(ls.get(0), dtf);
             Double price = !ls.get(1).trim().isEmpty() ? Double.parseDouble(ls.get(1)) : null;
+            Double RSI = !ls.get(4).trim().isEmpty() ? Double.parseDouble(ls.get(4)) : null;
+            Double KSTOCH = !ls.get(5).trim().isEmpty() ? Double.parseDouble(ls.get(5)) : null;
+            Double DSTOCH = !ls.get(6).trim().isEmpty() ? Double.parseDouble(ls.get(6)) : null;
+            Double ROC = !ls.get(7).trim().isEmpty() ? Double.parseDouble(ls.get(7)) : null;
+            Double MOMENTUM = !ls.get(8).trim().isEmpty() ? Double.parseDouble(ls.get(8)) : null;
+            Double WILLIAMS = !ls.get(9).trim().isEmpty() ? Double.parseDouble(ls.get(9)) : null;
+            Double sma1 = !ls.get(10).trim().isEmpty() ? Double.parseDouble(ls.get(10)) : null;
+            Double sma7 = !ls.get(11).trim().isEmpty() ? Double.parseDouble(ls.get(11)) : null;
+            Double sma30 = !ls.get(12).trim().isEmpty() ? Double.parseDouble(ls.get(12)) : null;
+            Double ema1 = !ls.get(13).trim().isEmpty() ? Double.parseDouble(ls.get(13)) : null;
+            Double ema7 = !ls.get(14).trim().isEmpty() ? Double.parseDouble(ls.get(14)) : null;
+            Double ema30 = !ls.get(15).trim().isEmpty() ? Double.parseDouble(ls.get(15)) : null;
+            Double wma1 = !ls.get(16).trim().isEmpty() ? Double.parseDouble(ls.get(16)) : null;
+            Double wma7 = !ls.get(17).trim().isEmpty() ? Double.parseDouble(ls.get(17)) : null;
+            Double wma30 = !ls.get(18).trim().isEmpty() ? Double.parseDouble(ls.get(18)) : null;
+            Double dema1 = !ls.get(19).trim().isEmpty() ? Double.parseDouble(ls.get(19)) : null;
+            Double dema7 = !ls.get(20).trim().isEmpty() ? Double.parseDouble(ls.get(20)) : null;
+            Double dema30 = !ls.get(21).trim().isEmpty() ? Double.parseDouble(ls.get(21)) : null;
+            Double tema1 = !ls.get(22).trim().isEmpty() ? Double.parseDouble(ls.get(22)) : null;
+            Double tema7 = !ls.get(23).trim().isEmpty() ? Double.parseDouble(ls.get(23)) : null;
+            Double tema30 = !ls.get(24).trim().isEmpty() ? Double.parseDouble(ls.get(24)) : null;
 
-            Map<String, Double> indicators = getStringDoubleMap(ls);
+            return Optional.of(
+                    new TAPoint
+                            (date, price, RSI, KSTOCH, DSTOCH, ROC, MOMENTUM, WILLIAMS, sma1, sma7, sma30, ema1, ema7,
+                                    ema30, wma1, wma7, wma30, dema1, dema7, dema30, tema1, tema7, tema30)
+            );
 
-            return Optional.of(new TAPoint(date, price, indicators));
         } catch (NumberFormatException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private static Map<String, Double> getStringDoubleMap(List<String> ls) {
-        Map<String, Double> indicators = new HashMap<>();
-        String[] indicatorsInOrder = {
-                "RSI", "KSTOCH", "DSTOCH", "ROC", "MOMENTUM", "WILLIAMS", "sma1", "sma7", "sma30", "ema1", "ema7",
-                "ema30", "wma1", "wma7", "wma30", "dema1", "dema7", "dema30", "tema1", "tema7", "tema30"};
-        for (int i = 4, j = 0; j < indicatorsInOrder.length; i++, j++) {
-            // indexing the csv from 4, and indexing the indicator names above from 0
-            if (!ls.get(i).trim().isEmpty()) {
-                indicators.put(
-                        indicatorsInOrder[j],
-                        Double.parseDouble(ls.get(i))
-                );
-            }
-        }
-        return indicators;
     }
 }
